1e509452feac6dedc564181856dd662d45e1bdf9
evitawang079-creator <evitawang079@gmail.com>
2026-07-05 14:18:17 +0700

    update

--- hospital_app.py
+++ hospital_app.py
@@ -163,12 +163,12 @@ with st.form("triage_form"):
     # TODO (text/color): replace CODENO14 and COLOR3.
     st.markdown("""
     <!-- COLOR3 -->
-    <div style="background:#f60909;border:1px solid #cbdc0d;border-radius:14px;
+    <div style="background:#31df0e;border:1px solid #cbdc0d;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
             <span style="background:#25f20a;color:white;border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">2</span>
-            <span style="font-size:16px;font-weight:600;color:#105efa;">CODENO14</span>
+            <span style="font-size:16px;font-weight:600;color:#ffffff;">How long have you had these symptoms?</span>
         </div>
     </div>
     """, unsafe_allow_html=True)
